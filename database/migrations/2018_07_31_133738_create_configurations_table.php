0dac2dc0873d47580a30f63a2c09f521a5623e20
quanghv96 <quanghavan96@gmail.com>
2018-07-31 16:33:17 +0700

    create migration part2

---
+++ database/migrations/2018_07_31_133738_create_configurations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConfigurationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('configurations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id')->unsigned()->unique();
+            $table->foreign('product_id')->references('id')->on('products')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->string('screen', 128);
+            $table->string('os', 128);
+            $table->string('back_camera', 100);
+            $table->string('front_camera', 100);
+            $table->string('ram', 100);
+            $table->string('memory', 100);
+            $table->string('battery_capacity', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('configurations');
+    }
+}
