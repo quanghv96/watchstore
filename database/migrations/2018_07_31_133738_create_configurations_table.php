17cd214439e042bdf152489cf71cd925db99921b
quanghv96 <quanghavan96@gmail.com>
2018-07-31 21:43:47 +0700

    create migration

--- database/migrations/2018_07_31_133738_create_configurations_table.php
+++ database/migrations/2018_07_31_133738_create_configurations_table.php
@@ -17,13 +17,13 @@ class CreateConfigurationsTable extends Migration
             $table->increments('id');
             $table->integer('product_id')->unsigned()->unique();
             $table->foreign('product_id')->references('id')->on('products')->onUpdate('CASCADE')->onDelete('CASCADE');
-            $table->string('man_hinh', 128);
-            $table->string('he_dieu_hanh', 128);
-            $table->string('camera_sau', 100);
-            $table->string('camera_truoc', 100);
+            $table->string('screen', 128);
+            $table->string('os', 128);
+            $table->string('back_camera', 100);
+            $table->string('font_camera', 100);
             $table->string('ram', 100);
-            $table->string('bo_nho', 100);
-            $table->string('dung_luong_pin', 100);
+            $table->string('memory', 100);
+            $table->string('battery_capacity', 100);
             $table->timestamps();
         });
     }
